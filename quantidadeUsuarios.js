bf35cd554c76e94d26ad5a74ae52348bca2a18ed
morango9 <105944114+morango9@users.noreply.github.com>
2024-10-18 08:24:38 -0300

    Update quantidadeUsuarios.js

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,23 +1,38 @@
-async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero_usuarios.json'
-    const res = await fetch(url)
-    console.log(res)
-    const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
+import { getCSS } from "./common.js";
+
+    async function quantidadeUsuarios() {
+      const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+      const res = await fetch(url)
+      const dados = await res.json()
+      const nomeDasRedes = Object.keys(dados)
+      const quantidadeDeUsuarios = Object.values(dados)
+    
       
     const data = [
         {
           x: nomeDasRedes,
-          y: quantidadeUsuarios,
+          y: quantidadeDeUsuarios,
           type: 'bar'
+          marker: {
+            color: getCSS('--primary-color')
+          }
+
         }
       ]
+
+      const layout = {
+        plot_bgcolor: getCSS('--bg-color')
+        paper_bgcolor: getCSS('--bg-color')
+        title: {
+          text: 'Redes sociais com mais usuários no mundo'
+        }
+    }
+    
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, data , layout)
 
   }
 
