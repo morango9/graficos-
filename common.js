649e2c65b85cb9f353f59c56d8fbd3a2c882c357
morango9 <105944114+morango9@users.noreply.github.com>
2024-10-25 11:05:27 -0300

    Update common.js

--- common.js
+++ common.js
@@ -2,10 +2,9 @@ const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel)
   };
   
-
-  
-const tickconfig = {
-  color
+  const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
 }
-
   
